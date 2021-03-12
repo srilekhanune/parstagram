1d8828162fb9c3dd15db1aad1a3bb41e265ff7f3
Srilekha Nune <srilekhanune@gmail.com>
2021-03-11 21:06:38 -0800

    Parstagram

--- parstagram/AppDelegate.swift
+++ parstagram/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Parse
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+       
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "YjRx868rUp4Cw19J3MvWTPsO7ztEdqOaYTytT5Uq" // <- UPDATE
+                $0.clientKey = "wen7vIt796RcSmUNoU1TiZIgTcTFGO7td6s07w0a" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
---
+++ parstagram/CameraViewController.swift
@@ -0,0 +1,84 @@
+//
+//  CameraViewController.swift
+//  parstagram
+//
+//  Created by Srilekha Nune on 3/11/21.
+//
+
+import UIKit
+import AlamofireImage
+import Parse
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var ImageView: UIImageView!
+    
+    @IBOutlet weak var commentField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func onSubmitButton(_ sender: Any) {
+        
+        let post = PFObject(className: "Posts")
+        
+        post["caption"] = commentField.text!
+        post["author"] = PFUser.current()!
+        
+        let imageData = ImageView.image!.pngData()
+        let file = PFFileObject(data: imageData!)
+        
+        
+        post["image"] = file
+        post.saveInBackground { (success, error) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print ("saved!")
+            } else {
+                print("error")
+            }
+        }
+    }
+    
+    
+    @IBAction func onCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af.imageScaled(to: size)
+        
+        ImageView.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
